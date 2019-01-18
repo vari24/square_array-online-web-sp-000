ab9dff60ca2495e1cd15fd13ae697ddbb76d2182
Sara Varisara <varisara.n@gmail.com>
2019-01-18 08:16:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -8,8 +8,8 @@ square_array(numbers)
 def square_array(array)
   new_a = []
   array.each do |numbers|
-    squared = numbers ** 2
-    new_a.push(squared)
+    expo = numbers ** 2
+    new_a.push(expo)
   end
   return new_a
 end
