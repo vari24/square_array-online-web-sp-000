9b4e8d6b40572d54035ac978d243bc77a7808b2e
Sara Varisara <varisara.n@gmail.com>
2019-01-18 08:20:43 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,14 @@
+numbers = [1,2,3]
 def square_array(array)
-  new_squared = []
-  array.each { |x| new_squared.push x ** 2 }
-  return new_squared
+  array.collect {|numbers| numbers ** 2}
+end
+
+
+def square_array(array)
+  new_a = []
+  array.each do |numbers|
+    expo = numbers ** 2
+    new_a.push(expo)
+  end
+  return new_a
 end
